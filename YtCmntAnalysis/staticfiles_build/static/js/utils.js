a963c9956478bef9ece4feb0bd9e0759031d61d0
Mathana Mathav <asmathav01@gmail.com>
2023-05-24 15:13:15 +0530

    bullet points fix 🧹

--- YtCmntAnalysis/staticfiles_build/static/js/utils.js
+++ YtCmntAnalysis/staticfiles_build/static/js/utils.js
@@ -70,7 +70,7 @@ $(window).on("load", function () {
           url: youtubeUrl,
         },
         success: function (response) {
-          console.log(response);
+          // console.log(response);
           document.getElementById("result-container").style.display = "block";
           document.getElementById("animationWindow").style.display = "none";
           const video_name = document.getElementById("video_name");
@@ -90,13 +90,19 @@ $(window).on("load", function () {
           const summary_info = document.getElementById("summary_info");
           const summary_text = document.getElementById("summary_text");
 
-          if (
-            !response.hasOwnProperty("Error")
-          ) {
-            summary_info.innerHTML =
-              "summary : Yeah bro it's a " +
-              response["video_analysis"]["Sentiment_summary"]["sentiment"] +
-              " video!!";
+          if (!response.hasOwnProperty("Error")) {
+            summary_info_text = "<span>summary : Yeah bro it's a " +
+            response["video_analysis"]["Sentiment_summary"]["sentiment"] +
+            " video!! </span>";
+            if (response["video_analysis"]["Sentiment_summary"]["sentiment"] === 'invalid') {
+              summary_info_text +=
+                '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="flex-shrink-0 w-5 h-5 text-red-500 dark:text-red-400"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /> </svg>';
+            } else {
+              summary_info_text +=
+                '<svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>';
+            }              
+            summary_info.innerHTML = summary_info_text;
+
             html = "<ul> comments with strong reactions : <br/>";
             for (
               var i = 0;
@@ -115,8 +121,8 @@ $(window).on("load", function () {
                 ][1];
 
               html +=
-                "<li class=\"flex items-center space-x-3 space-y-1\">" +
-                '<svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>' +
+                '<li class="flex items-center space-x-3 space-y-1">' +
+                '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="flex-shrink-0 w-5 h-5 text-orange-600 dark:text-orange-500"> <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" /></svg>' +
                 "<span>" +
                 comment +
                 " - Score: " +
@@ -126,20 +132,15 @@ $(window).on("load", function () {
             }
             html += "</ul>";
             summary_text.innerHTML = html;
-          } else if (
-            response["Error"] === 500 
-          ) {
+          } else if (response["Error"] === 500) {
             summary_info.innerHTML =
-              "Sorry bro, " +
-              "InSufficient Data to make an Analysis"
-              " for this video.";
+              "Sorry bro, " + "InSufficient Data to make an Analysis";
+            (" for this video.");
             summary_text.innerHTML = "";
-          } else if( response["Error"] === 403
-        ){
+          } else if (response["Error"] === 403) {
             document.getElementById("output").style.visibility = "collapse";
             alert("Comments are disabled for this video!");
-          }
-          else {
+          } else {
             document.getElementById("output").style.visibility = "collapse";
             alert("Video not found");
           }
