9cf5994b1d0545e849e222e92a47bb9d83a97137
Jawahar Jeeva <rjeprof@gmail.com>
2023-05-30 18:23:01 +0530

    vercel config

--- YtCmntAnalysis/build_files.sh
+++ YtCmntAnalysis/build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
 python -m pip install -r requirements.txt
-mkdir staticfiles_build/
+mkdir staticfiles_build/static
 python manage.py collectstatic --noinput --clear
 echo " BUILD END"
 
