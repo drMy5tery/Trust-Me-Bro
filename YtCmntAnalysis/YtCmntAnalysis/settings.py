116e46dc3bcd50bb13a6c60a84053cbd485fd2b8
Jawahar Jeeva <rjeprof@gmail.com>
2023-05-30 18:34:11 +0530

    vercel config

--- YtCmntAnalysis/YtCmntAnalysis/settings.py
+++ YtCmntAnalysis/YtCmntAnalysis/settings.py
@@ -81,12 +81,12 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 # Password validation
