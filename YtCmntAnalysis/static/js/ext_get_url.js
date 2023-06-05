f7519d8500b4f476dab94a8bcd6571b14ea0ffc5
Jawahar Jeeva <rjeprof@gmail.com>
2023-06-05 23:59:33 +0530

    function added to get current url

---
+++ YtCmntAnalysis/static/js/ext_get_url.js
@@ -0,0 +1,9 @@
+
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
+function(tabs){
+	getCurrentURL(tabs[0].url);
+});
+
+function getCurrentURL(tab){
+	currentURL = tab;
+}
