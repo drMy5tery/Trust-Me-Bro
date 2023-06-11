4a7f26c3eecbbf7a2fd3bdd004b80b32df9e3d34
Mathana Mathav <asmathav01@gmail.com>
2023-06-11 16:30:12 +0530

    railaway deploy check

--- YtCmntAnalysis/YtCmntAnalysis/urls.py
+++ YtCmntAnalysis/YtCmntAnalysis/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path("", include("Analysis.urls")),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
